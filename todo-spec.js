221e6f09520c30da4340c73dbcd204b5646cddf2
cacdcanada <hicham.jellab@ca.com>
2017-06-23 14:46:53 -0400

    Delete todo-spec.js

--- todo-spec.js
+++
@@ -1,8 +0,0 @@
-describe('CDBU Modern Software Factory UI Test - Canada Demo Environment', function() {
-  it('should add a todo', function() {
-    browser.ignoreSynchronization = true;
-    browser.get('http://canada-preprod.cdbu.io');
-    element(by.css('[name="name"]')).sendKeys('Selenium Test User');
-    element(by.buttonText('I\'m In!')).click();
-    });
-});
